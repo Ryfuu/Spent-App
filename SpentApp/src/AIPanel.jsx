95ac0ca52ce9e0e01af8058e8defa1bdaccb6b1e
Ryfuu <erokun26@gmail.com>
2026-04-28 17:58:07 +0800

    Fix ESLint: hapus unused vars

--- SpentApp/src/AIPanel.jsx
+++ SpentApp/src/AIPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { LuSparkles, LuSend } from 'react-icons/lu';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
