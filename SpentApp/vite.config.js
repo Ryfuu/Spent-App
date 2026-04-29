d3616d7c884449dc56570d3ca7a881187c664aaf
Ryfuu <erokun26@gmail.com>
2026-04-29 11:53:40 +0800

    fixing some bug

--- SpentApp/vite.config.js
+++ SpentApp/vite.config.js
@@ -4,15 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-build: {
-    rollupOptions: {
-      external: [/^react-icons/], 
-    },
-  },
-optimizeDeps: {
-    include: ['react-icons', '@google/generative-ai'],
-  },
 })
