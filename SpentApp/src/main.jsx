e211bd87799b2dc6e9520ab617a0bf4bdfacf93e
Ryfuu <erokun26@gmail.com>
2026-04-29 11:02:50 +0800

    idk what im doing

--- SpentApp/src/main.jsx
+++ SpentApp/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Spent from './assets/Spent.jsx';
+import Spent from './Spent.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Spent />
-  </StrictMode>,
 )
