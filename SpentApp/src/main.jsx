88fdc5079bfa81cd8349e8538e4ec2d919bf5a3c
Ryfuu <erokun26@gmail.com>
2026-04-29 10:39:45 +0800

    bug fixiing

--- SpentApp/src/main.jsx
+++ SpentApp/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Spent from "./Spent";
+import Spent from './Spent.jsx';
 
 
 
@@ -10,4 +10,3 @@ createRoot(document.getElementById('root')).render(
     <Spent />
   </StrictMode>,
 )
-
